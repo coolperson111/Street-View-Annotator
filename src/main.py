a2a1d6dc74d70e1c2889b6743b936b493bec21ae
Malhar Bhise <74235974+coolperson111@users.noreply.github.com>
2025-04-07 04:20:53 +0530

    small changes (#21)

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ from ui.gui import MainWindow
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    window = MainWindow(30.71097392052617, 76.80089082307204)
+    window = MainWindow(30.71097990326242, 76.8009197345154) # v3 road sec 28/29
+
     offset = 20
     window.setGeometry(0 + offset, 20 + offset, 1300 + offset, 900 + offset)
     window.show()
